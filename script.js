b61fb89c640a013cb38b44fa33179cd15735a0e8
a24sergiobc <a24sergiobc@iessanclemente.net>
2024-12-05 11:12:13 +0100

    update left

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 'use strict';
 
+const divLista = document.getElementById('lista');
+const btn_engadir = document.getElementById('btn_engadir');
+const btn_borrar = document.getElementById('btn_borrar');
+const txtb_filter = document.getElementById('txtb_filter');
+
 function actualizarListaElementos() {
-    let divLista = document.getElementById('lista');
     [...divLista.childNodes].map((node) => {
         divLista.removeChild(node);
+    });
+
+    let listaParseada = JSON.parse(localStorage.getItem('lista'));
+    listaParseada.map((elemento) => {
+        engadirElementoHTML(elemento);
     });
 }
 
 function engadirElementoHTML(productoEngadir) {
-    let divLista = document.getElementById('lista');
     let divElemento = document.createElement('div');
     divElemento.setAttribute('class', 'divProducto');
     let span1 = document.createElement('span');
@@ -24,71 +32,83 @@ function engadirElementoHTML(productoEngadir) {
 }
 
 function engadirElementoLocalStorage(producto) {
-    let lista = localStorage.getItem();
-    let listaParseada = JSON.parse(lista);
+    let listaParseada = JSON.parse(localStorage.getItem('lista'));
     listaParseada.push(producto);
     localStorage.setItem('lista', JSON.stringify(listaParseada));
 }
 
-function eliminarElementoHTML(divLista, divElemento) {
+function eliminarElementoHTML(divElemento) {
     divLista.removeChild(divElemento);
 }
 
-function eliminarElementoLocalStorage(posicion) {
-    console.log(localStorage.key(posicion));
-
-    localStorage.removeItem(posicion);
+function eliminarElementoLocalStorage(producto) {
+    let listaParseada = JSON.parse(localStorage.getItem('lista'));
+    let index = listaParseada.indexOf(producto);
+    if (index > -1) {
+        listaParseada.splice(index, 1);
+    }
+    localStorage.setItem('lista', JSON.stringify(listaParseada));
 }
 
-function filtro(divLista, contenido) {
-    [...divLista.childNodes].map(elemento, () => {});
-    // let index = [...divLista.childNodes].findIndex((elemento) => {
-    //     return elemento == event.target.closest('div');
-    // });
-}
+//**************************
 
-// Object.assign({}, localStorage)
-// *************************************************************
+function btn_engadirOnClickListener() {
+    if (document.getElementById('txtb_elemento').value != '') {
+        let listaParseada = JSON.parse(localStorage.getItem('lista'));
+        let productoEngadir = document.getElementById('txtb_elemento').value;
 
-const divLista = document.getElementById('lista');
-
-if (localStorage.getItem('lista') == null) {
-    let listaVacia = [];
-    localStorage.setItem('lista', JSON.stringify(listaVacia));
-}
-
-//Ocultar boton borrar todo
-
-actualizarListaElementos();
-
-//Click no botón engadir
-document.getElementById('btn_engadir').addEventListener('click', () => {
-    let productoEngadir = document.getElementById('txtb_elemento').value;
-    engadirElementoLocalStorage(productoEngadir);
-    engadirElementoHTML(productoEngadir);
-});
-
-//Click no botón borrar todo
-document.getElementById('btn_borrar').addEventListener('click', () => {
-    localStorage.clear();
-    actualizarListaElementos();
-});
-
-//Click no botón de borrar elemento
-divLista.addEventListener('click', (event) => {
-    if (event.target.tagName == 'SPAN' && event.target.innerText == '\u2715') {
-        if (confirm('¿Quiere borrar el artículo?')) {
-            let index = [...divLista.childNodes].findIndex((elemento) => {
-                return elemento == event.target.closest('div');
-            });
-
-            eliminarElementoLocalStorage(index + 1);
-            eliminarElementoHTML(divLista, [...divLista.childNodes][index]);
+        if ([...listaParseada].indexOf(productoEngadir) == -1) {
+            engadirElementoLocalStorage(productoEngadir);
+            engadirElementoHTML(productoEngadir);
+        } else {
+            alert('O producto xa está na lista!');
         }
     }
-});
+}
 
-//Filtro
-document.getElementById('txtb_filter').addEventListener('input', () => {
-    filtro(divLista);
-});
+function btn_borrarOnClickListener() {
+    localStorage.clear();
+    let listaVacia = [];
+    localStorage.setItem('lista', JSON.stringify(listaVacia));
+    actualizarListaElementos();
+}
+
+function btn_borrar_elementoOnClickListener(event) {
+    if (event.target.tagName == 'SPAN' && event.target.innerText == '\u2715') {
+        if (confirm('¿Quiere borrar el artículo?')) {
+            let divPadre = event.target.closest('div');
+            eliminarElementoLocalStorage(divPadre.childNodes[0].innerText);
+            eliminarElementoHTML(divPadre);
+        }
+    }
+}
+
+function txtb_filterOnTextChangedListener() {
+    let contenido = txtb_filter.value;
+    const filtro = new RegExp(contenido, 'i');
+
+    for (const element of divLista.children) {
+        if (element.querySelector('span').innerText.match(filtro)) {
+            element.classList.remove('ocultar');
+        } else {
+            element.classList.add('ocultar');
+        }
+    }
+}
+
+function main() {
+    if (localStorage.getItem('lista') == null) {
+        let listaVacia = [];
+        localStorage.setItem('lista', JSON.stringify(listaVacia));
+    }
+
+    actualizarListaElementos();
+
+    btn_engadir.addEventListener('click', btn_engadirOnClickListener);
+    btn_borrar.addEventListener('click', btn_borrarOnClickListener);
+    divLista.addEventListener('click', btn_borrar_elementoOnClickListener);
+
+    txtb_filter.addEventListener('input', txtb_filterOnTextChangedListener);
+}
+
+main();
